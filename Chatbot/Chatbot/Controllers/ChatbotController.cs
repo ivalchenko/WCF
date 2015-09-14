2574287b518ce3bdec546a6bf615ad6673d225b5
ivalchenko <i.valchenk@hotmail.com>
2015-09-14 20:33:35 +0300

    Client

---
+++ Chatbot/Chatbot/Controllers/ChatbotController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chatbot.Controllers
+{
+    public class ChatbotController : Controller
+    {
+        //ClientAuthorReference.ServiceAuthorClient obj = new ClientAuthorReference.ServiceAuthorClient();
+
+        ServiceReference1.IService1 obj = new ServiceReference1.IService1();
+
+        // GET: Chatbot
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Chatbot/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Chatbot/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Chatbot/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Chatbot/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Chatbot/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Chatbot/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Chatbot/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
